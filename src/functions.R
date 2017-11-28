42e18bfd26c3b89374d37e4ce92eb57411aec7dc
dschoenig <daniel.schoenig@posteo.de>
2017-11-28 17:53:10 +0100

    Added todos

--- src/functions.R
+++ src/functions.R
@@ -1,2 +1,8 @@
 # R functions to interact with the evidence assessment database
 library()
+
+# TODO
+# Create study, assessor, assessment, respectively
+# Get IDs for study, assessor, assessment, respectively
+# Create different template dataframe to enter data
+# Calculate LoE based on answers to questions
