f217aa40b0416a1b6333798fdc8d462bee6453c6
dschoenig <daniel.schoenig@posteo.de>
2018-01-25 12:29:41 +0100

    create user with restricted privileges

--- src/db_users_mysql.sql
+++ src/db_users_mysql.sql
@@ -3,4 +3,7 @@ DROP USER IF EXISTS 'evidence_admin'@'localhost';
 CREATE USER 'evidence_admin'@'localhost' IDENTIFIED BY 'biometry101';
 GRANT ALL PRIVILEGES ON evidence_assessment.* TO 'evidence_admin'@'localhost';
 
+CREATE USER 'evidence_user'@'localhost';
+GRANT SELECT, INSERT ON evidence_assessment.* TO 'evidence_user'@'localhost';
+
 FLUSH PRIVILEGES;
