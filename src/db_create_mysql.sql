cbe143be41660b29050a3002fb6936f7503cf772
dschoenig <daniel.schoenig@posteo.de>
2018-01-25 23:54:11 +0100

    corrected typo in key constraints

--- src/db_create_mysql.sql
+++ src/db_create_mysql.sql
@@ -88,7 +88,7 @@ CREATE TABLE assessments(
 
   /* Keys */
   PRIMARY KEY(assessment_id),
-  FOREIGN KEY(assessor_id) REFERENCES assessors(assessor_id)
+  FOREIGN KEY(assessor_id) REFERENCES assessors(assessor_id),
   UNIQUE KEY assessor_date (assessor_id, date_entered)
 );
 
